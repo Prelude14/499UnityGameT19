c77c9abdc3636a91892ce3af073b5808c823a47f
Brenner <43012551+Prelude14@users.noreply.github.com>
2024-01-28 18:41:27 -0800

    trying to upload progress for multiplayer build

--- My project (4)/Assets/playerDeck.cs
+++ My project (4)/Assets/playerDeck.cs
@@ -16,6 +16,10 @@ public class playerDeck : NetworkBehaviour
     public static string playerColour;
     public string gameDeckCombo = "";
     public bool gameStarted = false;
+<<<<<<< Updated upstream
+=======
+    public bool sent1 = false;
+>>>>>>> Stashed changes
 
     // Start is called before the first frame update
     void Start()
@@ -40,19 +44,48 @@ public class playerDeck : NetworkBehaviour
         //tell server to change size of deck and the text displaying how many cards are left in it, as well as deal new cards if turn has changed
         //PlayerManager.CmdUpdate();
 
+<<<<<<< Updated upstream
         if (!gameStarted) //check if game has started yet, if not, dont do anything
         {
             gameDeckCombo = PlayerManager.clientDecks;//get sync variable from network manager
 
             if (!gameDeckCombo.Equals("Error 1: Not enough players/strings.") || !gameDeckCombo.Equals("Error 2: Too many players/strings.") ) //if game started with valid combo, ask server to deal and then start game
+=======
+
+        if (!gameStarted) //check if game has started yet, if not, dont do anything
+        {
+
+            gameDeckCombo = PlayerManager.clientDecks;//get sync variable from network manager
+           
+
+            //on update each frame, check if client deck string is still empty, meaning no one has sent their colour to server yet, then send your colour
+            if (PlayerManager.clientDecks.Equals("") && sent1 == false)
+            {
+                PlayerManager.servercheckchangecombo(playerColour); //send playermanager the deck colour to be used, once 2 clients have run this, the server should deal out car
+                sent1 = true;
+                //Debug.Log("client sent this colour to server 1 time:" + playerColour);//check to see it updating
+            }
+            else if (sent1 == true && ( PlayerManager.clientDecks.Equals("BLACK") || PlayerManager.clientDecks.Equals("R") || PlayerManager.clientDecks.Equals("W") || PlayerManager.clientDecks.Equals("BLUE") ) )
+            {
+                //if client has already sent its colour to server, and there is one colour inside the shared string, then this client will wait until the other player connects
+            }
+            else if (!PlayerManager.clientDecks.Equals("Error 1: Not enough players/strings.") || !PlayerManager.clientDecks.Equals("Error 2: Too many players/strings.") ) //if game started with valid combo, ask server to deal and then start game
+>>>>>>> Stashed changes
             {
                 PlayerManager.CmdDraw(2);
                 gameStarted = true;
             }
+<<<<<<< Updated upstream
             else if (gameDeckCombo.Equals("Error 1: Not enough players/strings.") ) //if game isn't started yet, and it doesn't have enough players yet for a valid combo, keep getting server to check each time update runs
             {
                 PlayerManager.CmdCreateDeck();
             }
+=======
+            //else if (gameDeckCombo.Equals("Error 1: Not enough players/strings.") ) //if game isn't started yet, and it doesn't have enough players yet for a valid combo, keep getting server to check each time update runs
+            //{
+            //    PlayerManager.CmdCreateDeck();
+            //}
+>>>>>>> Stashed changes
         }
 
         //reduce number of "visible" cards on the -deck- stack
