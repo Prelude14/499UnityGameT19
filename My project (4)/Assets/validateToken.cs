5ea9c57253d7a03d885cde2a226abfe1f3738e29
lhenon999 <77656081+lhenon999@users.noreply.github.com>
2024-03-04 13:44:46 -0800

    validate sql connection

---
+++ My project (4)/Assets/validateToken.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class validateToken : MonoBehaviour
+{
+
+    public Text username_email;
+    public Text token;
+
+    public void CallValidate()
+    {
+        StartCoroutine(validate());
+    }
+
+    IEnumerator validate()
+    {
+        //make Form to take the user's input 
+        WWWForm form = new WWWForm();
+        form.AddField("username", username_email.text);
+        form.AddField("token", token.text);
+
+        //connect to url of our database's php file, PASS FORM TO URL
+        WWW www = new WWW("http://localhost/sqlconnect/validate.php", form);
+        yield return www; //tell Unity to yield running the rest of the game till it gets this info from the url
+
+        //Error check what our PHP file returned, index 0 should be the first character, 0 means everything worked perfectly
+        if (www.text[0] == '0')
+        {
+            Debug.Log("Token Valid");
+        }
+        else
+        {
+
+            Debug.Log("Validation FAILED. Error Code: " + www.text);
+        }
+    }
+}
