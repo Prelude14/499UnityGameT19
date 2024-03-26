e68c07a5bc5cb13c6643223bb8cc5bca73fb4bbe
Brenner <43012551+Prelude14@users.noreply.github.com>
2024-03-26 13:45:15 -0700

    draft of enemyHealth.cs's using Server's PlayerHealths
    Incorporated the script to use SharedVarManager's p1Health and p2Health values to display the opponent's health properly. Will probably need to turn the script into a NetworkBehaviour script and include Mirror, but will find out when I can run the script on my machine.

--- My project (4)/Assets/enemyHealth.cs
+++ My project (4)/Assets/enemyHealth.cs
@@ -8,10 +8,11 @@ public class enemyHealth : MonoBehaviour
     // Start is called before the first frame update
     public static float maxHp;
     public static float HPStatic;
-    public float hp;
+    public float hp; // actual health point count
     public Image health;
-    public Text hpText;
-    public GameObject glow;
+    public Text hpText; //display enemy health
+    public GameObject glow; //orange glow around enemy's health 
+    
     void Start()
     {
         maxHp = 30;
@@ -21,9 +22,13 @@ public class enemyHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        glow.SetActive(false);
+        glow.SetActive(false); //keep glow off
+
+        SetUpOppHealth(); //find out how much health to give the opponent -- assigns HPStatic to match server's values
+        
         //Fill health bar to this much
-        hp = HPStatic;
+        hp = HPStatic; //this should be assigned by the server in setUpOppHealth
+        
         health.fillAmount = hp / maxHp; //percentage of fill amount
         if (hp >= maxHp)
         {
@@ -40,6 +45,30 @@ public class enemyHealth : MonoBehaviour
             glow.SetActive(false);
         }
 
+    }
+    public void SetUpOppHealth()
+    {
+        //find sharedvar game object in scene at runtime, CHECK for player's health
+        GameObject sharedVarManagerObj = GameObject.Find("SharedVarManager");
+        SharedVarManager sharedVarManager = sharedVarManagerObj.GetComponent<SharedVarManager>();
+
+        //locate the PlayerManager in the Client, need to check if it is player 1 or 2
+        NetworkIdentity networkIdentity = NetworkClient.connection.identity;
+        PlayerManager = networkIdentity.GetComponent<PlayerManager>();
+
+        if (PlayerManager.isPlayerOne == true && PlayerManager.isPlayerTwo == false) //if I'm player ONE 
+        {
+            float opponentsHealth = sharedVarManager.p2Health; //set opp's health to equal that in the sharedVarManager script
+        }
+        else if (PlayerManager.isPlayerTwo == true && PlayerManager.isPlayerOne == false) //if I'm player TWO 
+        {
+            float opponentsHealth = sharedVarManager.p1Health; //set opp's health to equal that in the sharedVarManager script
+        }
+        else
+        {
+            float opponentsHealth = HPStatic; //if players aren't assigned yet, just assign it to default 30 until update works
+        }
+        HPStatic = opponentsHealth; //update the opponentHealth script's hp to actually match that of SharedVarManager(the server's value) 
     }
     public float getHealth()
     {
