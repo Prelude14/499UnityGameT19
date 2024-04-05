d122294c5dfdae010fc037d46d38c3c032610978
lhenon999 <77656081+lhenon999@users.noreply.github.com>
2024-04-04 17:06:22 -0700

    fixed logout bug

--- GitHub_int_project/Assets/MainMenu.cs
+++ GitHub_int_project/Assets/MainMenu.cs
@@ -6,6 +6,11 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+
+    void start(){
+        ActiveMainMenu();
+    }
+
     //need public variable that will track if the user logged in or not
     public GameObject guestbackground;
     public GameObject usersMMbackground;
